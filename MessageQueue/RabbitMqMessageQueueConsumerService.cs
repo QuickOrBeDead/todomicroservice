83815b444e3665241d22697a82807155283deb07
QuickOrBeDead <boraakgn@gmail.com>
2022-05-12 22:35:48 +0300

    add non generic consumer

--- MessageQueue/RabbitMqMessageQueueConsumerService.cs
+++ MessageQueue/RabbitMqMessageQueueConsumerService.cs
@@ -8,12 +8,17 @@
     using RabbitMQ.Client;
     using RabbitMQ.Client.Events;
 
-    public interface IMessageQueueConsumerService<out TModel> : IDisposable
+    public interface IMessageQueueConsumerService : IDisposable
+    {
+        void ConsumeMessage(Action<ReadOnlyMemory<byte>> consumeAction);
+    }
+
+    public interface IMessageQueueConsumerService<out TModel> : IMessageQueueConsumerService
     {
         void ConsumeMessage(Action<TModel> consumeAction);
     }
 
-    public class RabbitMqMessageQueueConsumerService<TModel> : IMessageQueueConsumerService<TModel>
+    public class RabbitMqMessageQueueConsumerService : IMessageQueueConsumerService
     {
         private readonly string _host;
 
@@ -25,15 +30,17 @@
 
         private readonly string _queue;
 
+        private readonly bool _declareQueue;
+
         private bool _disposed;
 
         private IConnection? _connection;
 
         private IModel? _channel;
 
-        private string _consumerTag;
+        private string? _consumerTag;
 
-        public RabbitMqMessageQueueConsumerService(string host, string userName, string password, string exchange, string queue)
+        public RabbitMqMessageQueueConsumerService(string host, string userName, string password, string exchange, string queue, bool declareQueue = true)
         {
             if (string.IsNullOrWhiteSpace(userName))
             {
@@ -65,9 +72,10 @@
             _password = password;
             _exchange = exchange;
             _queue = queue;
+            _declareQueue = declareQueue;
         }
 
-        public void ConsumeMessage(Action<TModel> consumeAction)
+        public void ConsumeMessage(Action<ReadOnlyMemory<byte>> consumeAction)
         {
             if (consumeAction == null)
             {
@@ -79,28 +87,28 @@
                 .WaitAndRetry(10, r => TimeSpan.FromSeconds(5))
                 .Execute(
                     () =>
-                        {
-                            var factory = new ConnectionFactory { HostName = _host, UserName = _userName, Password = _password };
-                            _connection = factory.CreateConnection();
-                            _channel = _connection.CreateModel();
+                    {
+                        var factory = new ConnectionFactory { HostName = _host, UserName = _userName, Password = _password };
+                        _connection = factory.CreateConnection();
+                        _channel = _connection.CreateModel();
 
-                            _channel.ExchangeDeclare(_exchange, "fanout", true, false, null);
+                        _channel.DeclareExchange(_exchange);
+
+                        if (_declareQueue)
+                        {
                             _channel.QueueDeclare(_queue, true, false, false, null);
                             _channel.QueueBind(_queue, _exchange, string.Empty);
+                        }
 
-                            var consumer = new EventingBasicConsumer(_channel);
-                            consumer.Received += (_, e) =>
-                                {
-                                    var model = JsonSerializer.Deserialize<TModel>(Encoding.UTF8.GetString(e.Body.Span));
-                                    if (model != null)
-                                    {
-                                        consumeAction(model);
-                                    }
+                        var consumer = new EventingBasicConsumer(_channel);
+                        consumer.Received += (_, e) =>
+                        {
+                            consumeAction(e.Body);
 
-                                    _channel.BasicAck(e.DeliveryTag, false);
-                                };
-                            _consumerTag = _channel.BasicConsume(_queue, false, consumer);
-                        });
+                            _channel.BasicAck(e.DeliveryTag, false);
+                        };
+                        _consumerTag = _channel.BasicConsume(_queue, false, consumer);
+                    });
         }
 
         protected virtual void Dispose(bool disposing)
@@ -131,6 +139,32 @@
         {
             Dispose(true);
             GC.SuppressFinalize(this);
+        }
+    }
+
+    public class RabbitMqMessageQueueGenericConsumerService<TModel> : RabbitMqMessageQueueConsumerService, IMessageQueueConsumerService<TModel>
+    {
+        public RabbitMqMessageQueueGenericConsumerService(string host, string userName, string password, string exchange, string queue, bool declareQueue = true) 
+            : base(host, userName, password, exchange, queue, declareQueue)
+        {
+        }
+
+        public void ConsumeMessage(Action<TModel> consumeAction)
+        {
+            if (consumeAction == null)
+            {
+                throw new ArgumentNullException(nameof(consumeAction));
+            }
+
+            ConsumeMessage(
+                m =>
+                    {
+                        var model = JsonSerializer.Deserialize<TModel>(Encoding.UTF8.GetString(m.Span));
+                        if (model != null)
+                        {
+                            consumeAction(model);
+                        }
+                    });
         }
     }
 }
