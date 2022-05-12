7ed3b8c6e409e0e5d178faaff89a056d006f8022
QuickOrBeDead <boraakgn@gmail.com>
2022-05-12 22:35:07 +0300

    create DeclareExchange extension method for consumer and publisher

---
+++ MessageQueue/ChannelExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace MessageQueue
+{
+    using RabbitMQ.Client;
+
+    internal static class ChannelExtensions
+    {
+        public static void DeclareExchange(this IModel channel, string exchange)
+        {
+            if (channel == null)
+            {
+                throw new ArgumentNullException(nameof(channel));
+            }
+
+            channel.ExchangeDeclare("unrouted", "fanout", true, false, null);
+            channel.QueueDeclare("unrouted", true, false, false, null);
+            channel.QueueBind("unrouted", "unrouted", string.Empty);
+
+            channel.ExchangeDeclare(
+                exchange,
+                "fanout",
+                true,
+                false,
+                new Dictionary<string, object> { { "alternate-exchange", "unrouted" } });
+        }
+    }
+}
