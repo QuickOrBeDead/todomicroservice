bfd423cf6cb96df200cda455630abb56609ece0b
QuickOrBeDead <boraakgn@gmail.com>
2022-06-18 21:06:27 +0300

    change basepath to api gateway url

--- TodoWebApp/src/api.ts
+++ TodoWebApp/src/api.ts
@@ -2,6 +2,6 @@ import { BaseAPI as TaskManagmentApi } from "./metadata/task-management-api/base
 
 export function createTaskManagementApi<T extends TaskManagmentApi>(ctor: { new (): T }) : T {
     const api = new ctor()
-    api["basePath"] = "http://localhost:8081"
+    api["basePath"] = "http://localhost:10000/task-management"
     return api
 }
